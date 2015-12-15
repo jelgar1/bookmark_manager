97e643f4a5c0e7911d9481908f027b374ee1e408
edwardkerry <edwardkerry@gmail.com>
2015-12-15 17:22:36 +0000

    WIP RACK ENV not working for dev/test separation

--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -1,6 +1,6 @@
 feature 'creating a link' do
   scenario 'saving a website url and title to bookmarks' do
-    visit '/links/new'
+    visit 'links/new'
     fill_in 'title', with: 'link2'
     fill_in 'url', with: 'www.internet.com'
     click_button 'Create bookmark'
